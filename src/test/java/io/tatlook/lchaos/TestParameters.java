785552ce75d9a701b41c6df2057640fa52c8b098
tatlook <youzhezhen@gmail.com>
2021-10-03 18:30:08 +0300

    test: isolate test rule files from source

--- src/test/java/io/tatlook/lchaos/TestParameters.java
+++ src/test/java/io/tatlook/lchaos/TestParameters.java
@@ -18,12 +18,6 @@
 
 package io.tatlook.lchaos;
 
-import static org.junit.Assert.fail;
-
-import java.io.File;
-import java.io.IOException;
-import java.io.PrintStream;
-
 import org.junit.Test;
 
 /**
@@ -34,83 +28,38 @@ public class TestParameters {
 
 	@Test
 	public void testNoFile() {
-		String[] args = {
-				"momo...';;;\"{}!323\n[]^<>^^**??\\/.", "dsakdkk"
-		};
-		App.main(args);
+		App.main(new String[] {
+			"momo...';;;\"{}!323\n[]^<>^^**??\\/.", "dsakdkk"
+		});
+		System.out.println("Working Directory = " + System.getProperty("user.dir"));
 	}
 
 	@Test
 	public void testFailureOption() {
-		String[] args = {
-				"--help", "dsakdkk"
-		};
-		App.main(args);
+		App.main(new String[] {
+			"--help", "dsakdkk"
+		});
 	}
 
 	@Test
 	public void testErrorFile() {
-		File file = new File("omomo");
-		try {
-			file.createNewFile();
-		} catch (IOException e) {
-			fail(e.getMessage());
-		}
-		String[] args = {
-				file.getName(), "dsakdkk"
-		};
-		App.main(args);
-		file.delete();
+		App.main(new String[] {
+			"errorfile.error_ext_name", "dsakdkk"
+		});
 	}
 	
 	@Test
 	public void testChaosFile() {
-		File file = new File("om.ch");
-		try {
-			file.createNewFile();
-			PrintStream out = new PrintStream(file);
-			out.println("1");
-			out.println("    0.0");
-			out.println("1 3");
-			out.println("    0.0 0.0 0.0");
-			out.println("1 3");
-			out.println("    0.0 0.0 0.0");
-			out.println("Comments!!!");
-			
-			out.close();
-		} catch (IOException e) {
-			fail(e.getMessage());
-		}
-		String[] args = {
-				file.getName(), "dsakdkk"
-		};
-		App.main(args);
-		file.delete();
+		App.main(new String[] {
+			"chaos_file.ch", "dsakdkk"
+		});
 	}
 	
 	@Test
 	public void testFractintIFSFile() {
-		File file = new File("omomo.ifs");
-		try {
-			file.createNewFile();
-			PrintStream out = new PrintStream(file);
-			out.println("demo } +++++ haha haha hahah");
-			out.println("demo {");
-			out.println();
-			out.println("; ignore !!!");
-			out.println("    0.0 0.0 0.0 0.0 0.0 0.0 0.0 ; yes");
-			out.println("    0.0 0.0 0.0 0.0 0.0 0.0 0.0 haha no !! no!! ");
-			out.println("} kljlkj");
-			
-			out.close();
-		} catch (IOException e) {
-			fail(e.getMessage());
-		}
-		String[] args = {
-				file.getName(), "dsakdkk"
-		};
-		App.main(args);
-		file.delete();
+		App.main(new String[] {
+			"ifs_file.ifs", "dsakdkk"
+		});
 	}
 
 }
